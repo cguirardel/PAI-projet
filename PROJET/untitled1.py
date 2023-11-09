d5cc534a7007640b9662927c1fc3351b79e5c82a
cguirardel <150354144+cguirardel@users.noreply.github.com>
2023-11-09 17:57:08 +0100

    Add files via upload
    Start of project

---
+++ PROJET/untitled1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  9 14:19:25 2023
+
+@author: colin.guirardel
+"""
+
+
+import pandas as pd
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
