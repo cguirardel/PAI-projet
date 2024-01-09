2920d0ee9ab5acc0e7a1b640496059e33ff2b682
Gabriel Lecarme <gabriel.lecarme@outlook.fr>
2024-01-09 15:33:15 +0100

    mise à jour

--- PROJET/Modules Traitements/traitementV0.py
+++ PROJET/Modules Traitements/traitementV0.py
@@ -10,13 +10,17 @@ https://www.kaggle.com/datasets/bhanupratapbiswas/olympic-data/
 import numpy as np
 import pandas
 
+SUMMER = 1
+WINTER = 2
+ALL_SEASON = 0
+
 olympics = pandas.read_csv("./data/dataset_olympics.csv")
 noc = pandas.read_csv("./data/noc_region.csv")
 
 lignes = olympics.index
 columns = olympics.columns
 
-def compteMedailles(csv, start_year, end_year, medal_type = 'All', edition = 0):
+def compteMedailles(df, start_year, end_year, medal_type = 'All', edition = ALL_SEASON):
     """_summary_
 
     Args:
@@ -33,22 +37,25 @@ def compteMedailles(csv, start_year, end_year, medal_type = 'All', edition = 0):
     Returns:
         _type_: _description_
     """
+
+    df = df[~df.Medal.isnan()]
+
     if not(medal_type in ["Gold", "Silver", "Bronze", "All"]):
         raise ValueError('medal_type must be in ["Gold", "Silver", "Bronze", "All"]')
     if not(edition in [0, 1, 2]):
         raise ValueError('edition must be an int = 0; 1 or 2')
     if medal_type == 'All':
-        gold = compteMedailles(csv, start_year, end_year, 'Gold', edition)
-        silver = compteMedailles(csv, start_year, end_year, medal_type = 'Silver', edition = edition)
-        bronze = compteMedailles(csv, start_year, end_year, medal_type = 'Bronze', edition = edition)
+        gold = compteMedailles(df, start_year, end_year, 'Gold', edition)
+        silver = compteMedailles(df, start_year, end_year, medal_type = 'Silver', edition = edition)
+        bronze = compteMedailles(df, start_year, end_year, medal_type = 'Bronze', edition = edition)
         compte = gold + silver + bronze
     else:
-        formatdf = csv.astype({'NOC' : 'category'}) # Permet de conserver les 'NOC' avec 0 médaille d'or
-        formatdf = formatdf[(csv.Year >= start_year) & (csv.Year <= end_year)]  # Filtrage par période
+        formatdf = df.astype({'NOC' : 'category'}) # Permet de conserver les 'NOC' avec 0 médaille d'or
+        formatdf = formatdf[(df.Year >= start_year) & (df.Year <= end_year)]  # Filtrage par période
         if edition != 0:
             season = ["Summer", "Winter"][edition - 1]
-            formatdf = formatdf[csv.Season == season]
-        compte = formatdf.loc[csv.Medal == medal_type, ["NOC", "Medal"]]   # Filtrage par médaille
+            formatdf = formatdf[df.Season == season]
+        compte = formatdf.loc[df.Medal == medal_type, ["NOC", "Medal"]]   # Filtrage par médaille
         dfgroupby = compte.groupby('NOC')    # Regroupement <pandas.core.groupby.generic.DataFrameGroupBy object at 0x000001A7E13A4D50>
         compte = dfgroupby.count()   # Comptage
     compte = compte.sort_values('Medal', ascending = False) # Tri
